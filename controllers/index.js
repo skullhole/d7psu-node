7edbd66c22ac7d228a848da30a8f64a9e832fcf2
skullhole <skullhole@gmail.com>
2017-11-20 00:18:28 +0700

    Complete version based on d7psu

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,13 @@
 var router = require('express').Router();
 
+/**
+ * Status XML.
+ */
+router.get('/status/:user/:repo', function (req, res) {
+  utility.status(req.params.user, req.params.repo, function (output) {
+    res.set('Content-Type', 'text/xml');
+    res.send(output);
+  })
+});
+
 module.exports = router;
